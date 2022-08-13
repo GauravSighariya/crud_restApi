82e46167dac1d0fbd114cde4b37d81fed626c3b8
GauravSighariya <84697605+GauravSighariya@users.noreply.github.com>
2022-08-12 22:39:25 -0700

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -6,22 +6,9 @@ from drf_yasg import openapi
 from rest_framework.documentation import include_docs_urls
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Product API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('product_api.urls')),
-    path('docs/', include_docs_urls(title='Product API')), # Api Documentation endpoint
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), # Api Documentation endpoint
-    
+    path('docs/', include_docs_urls(title='Product API')), # Api Documentation endpoint    
 ]
